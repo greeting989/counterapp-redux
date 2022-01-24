4705761da573b6b75ab8841b1a98491e9b0f89eb
Ayushee <ashrivastava0812@gmail.com>
2022-01-24 11:33:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useSelector ,useDispatch} from 'react-redux';
+import {incNumber,decNumber} from './actions/index';
+
 
 function App() {
+  const myState = useSelector((state)=>state.changeNumber)
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <input type="text" className='count' value={myState}/>
+        </div>
+          <div className="counterdiv">
+             <button className='btn' onClick={()=>dispatch(decNumber())}>Decrease</button>
+             <button className='btn' onClick={()=>dispatch(incNumber())}>Increment</button>
+          </div>
       </header>
     </div>
   );
---
+++ src/reducers/upDown.js
@@ -0,0 +1,13 @@
+let intialState = 0;
+const changeNumber = (state=intialState,action) =>{
+    switch(action.type){
+        case "INCREMENT":
+            return state+1;
+        case "DECREMENT":
+                return state-1;
+        default :
+                return state;
+    }
+}
+
+export default changeNumber;
